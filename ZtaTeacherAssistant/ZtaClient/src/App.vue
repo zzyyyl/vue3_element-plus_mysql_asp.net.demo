54795e51546a6403046db867456ace54cda371c7
zzyyyl <aysyxx53@hotmail.com>
2023-06-15 12:54:48 +0800

    style: format

--- ZtaTeacherAssistant/ZtaClient/src/App.vue
+++ ZtaTeacherAssistant/ZtaClient/src/App.vue
@@ -7,7 +7,7 @@ export default {
   components: {
     PaperBlock,
     HitokotoBlock
-},
+  },
   props: {},
   data() {
     return {
@@ -18,28 +18,28 @@ export default {
     }
   },
   created() {
-    const picTotal = 13;
-    let randBgIndex = Math.floor(Math.random() * picTotal) % picTotal;
-    document.body.style.backgroundImage = "url(backgroundpics/" + randBgIndex + ".jpg)";
-    document.body.style.backgroundPosition = "center center";
-    document.body.style.backgroundRepeat = "no-repeat";
+    const picTotal = 13
+    let randBgIndex = Math.floor(Math.random() * picTotal) % picTotal
+    document.body.style.backgroundImage = 'url(backgroundpics/' + randBgIndex + '.jpg)'
+    document.body.style.backgroundPosition = 'center center'
+    document.body.style.backgroundRepeat = 'no-repeat'
   }
 }
-
 </script>
 
 <template>
-  <nav class='maintitle'><ul>
-    <li class='EnglishP thick maintitletext'><a href='/'>ZTA</a></li>
-    <li v-for="(value, key) in models" :key="key" class="Code">
-      <a :href="value">{{ key }}</a>
-    </li>
-	</ul></nav>
+  <nav class="maintitle">
+    <ul>
+      <li class="EnglishP thick maintitletext"><a href="/">ZTA</a></li>
+      <li v-for="(value, key) in models" :key="key" class="Code">
+        <a :href="value">{{ key }}</a>
+      </li>
+    </ul>
+  </nav>
   <div class="transbox">
     <div class="sidebar"><hitokoto-block /></div>
     <div class="mainbar"><paper-block /></div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- ZtaTeacherAssistant/ZtaClient/src/components/items/PaperItem.vue
+++ ZtaTeacherAssistant/ZtaClient/src/components/items/PaperItem.vue
@@ -1,35 +1,32 @@
 <script lang="ts">
 export default {
-	props: [
-        'pid',
-        'pname',
-        'psource',
-        'pyear',
-        'ptype',
-        'level'
-    ],
+  props: ['pid', 'pname', 'psource', 'pyear', 'ptype', 'level'],
   emits: ['remove']
 }
 </script>
 
 <template>
   <li>
-    <div style="float: right; width: 20%; text-align: center;">
-    <el-button plain type="danger" @click="$emit('remove')">删 除</el-button>
+    <div style="float: right; width: 20%; text-align: center">
+      <el-button plain type="danger" @click="$emit('remove')">删 除</el-button>
     </div>
-    <div style="width: 80%;">
-    <pre>
-    论文编号：{{ pid }}
-    论文名称：{{ pname }}
-    论文来源：{{ psource }}
-    论文年份：{{ pyear }}
-    论文类别：{{ ptype }}
-    论文级别：{{ level }}
-    </pre>
+    <div style="width: 80%">
+      <pre>
+      论文编号： {{ pid }}
+      论文名称： {{ pname }}
+      论文来源： {{ psource }}
+      论文年份： {{ pyear }}
+      论文类别： {{ ptype }}
+      论文级别： {{ level }}
+      </pre>
     </div>
   </li>
 </template>
 
 <style scoped>
-button{width: 5rem;height: 2.5rem;margin:.4rem 1rem}
+button {
+  width: 5rem;
+  height: 2.5rem;
+  margin: 0.4rem 1rem;
+}
 </style>
